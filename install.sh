d9074a86ce88d8c7669e5ce2c17c53bba6044cd6
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2020-10-03 15:41:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+web_site=$1
+email=$2
+password_email=$3
+db_name=$4
+db_user=$5
+db_password=$6
 
 if [[ $# -eq 0 ]]; then
     echo "número de argumentos não bate"
@@ -50,7 +55,7 @@ install_runner(){
 }
 
 config_site(){
-    python3.8 helper.py $1 $2 $3 $4 $5 $6
+    python3.8 helper.py web_site email password_email db_name db_user db_password
 }
 
 install_python
