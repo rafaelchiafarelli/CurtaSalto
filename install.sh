945c3a5077fc0b72b3c5c646331615aa7a9e90ed
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2020-10-03 15:30:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,9 +22,9 @@ install_python(){
     sudo add-apt-repository ppa:deadsnakes/ppa
     sudo apt update
     sudo apt install python3.8 python3.8-dev python3.8-venv
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.5 1
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
-    sudo update-alternatives --set python3 /usr/bin/python3.8
+    sudo apt install python3.8-distutils
+    sudo apt install python3.8-venv python3.8-dev
+    python3.8 -m pip install --upgrade pip setuptools wheel
     pip3 install --upgrade pip
     pip3 install update pip
 }
